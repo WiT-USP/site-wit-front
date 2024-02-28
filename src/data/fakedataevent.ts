de7c4dcd16bf30b31f9896da2d0a87b5f552356a
NicolasFCruvinel <nicolasfcmsouza@gmail.com>
2024-02-28 14:43:54 -0300

    HotFix: alterando dados mockados do events para incluir as imagens.

---
+++ src/data/fakedataevent.ts
@@ -0,0 +1,34 @@
+import card_image from "../assets/img/card_image.png";
+
+export const fakeEvents = [
+  {
+    cardNumber: 1,
+    eventName: "DevCon 2024: The Future of Coding",
+    link_image: card_image,
+    isSelected: false,
+  },
+  {
+    cardNumber: 2,
+    eventName: "AI Summit: Exploring Artificial Intelligence",
+    link_image: card_image,
+    isSelected: false,
+  },
+  {
+    cardNumber: 3,
+    eventName: "CodeCamp: Mastering Web Development",
+    link_image: card_image,
+    isSelected: false,
+  },
+  {
+    cardNumber: 4,
+    eventName: "Cybersecurity Forum: Securing the Digital World",
+    link_image: card_image,
+    isSelected: false,
+  },
+  {
+    cardNumber: 5,
+    eventName: "Tech Startup Expo: Innovation Showcase",
+    link_image: card_image,
+    isSelected: false,
+  },
+];
--- src/pages/user-pages/events/index.tsx
+++ src/pages/user-pages/events/index.tsx
@@ -1,32 +1,31 @@
-import React from "react";
-import GlobalStyles from "../../../styles/GlobalStyles";
-import Header from "../../../components/user-components/computer-window/header";
-import Footer from "../../../components/user-components/computer-window/footer";
 import Filter from "../../../components/admin-components/search-bar";
 import CardsSet from "../../../components/user-components/computer-window/cards-set";
+import Footer from "../../../components/user-components/computer-window/footer";
+import Header from "../../../components/user-components/computer-window/header";
+import GlobalStyles from "../../../styles/GlobalStyles";
 
-import data from "../../../data/fakedataevent.json";
+import { fakeEvents } from "../../../data/fakedataevent";
 
-import {Container, HeaderContainer, FooterContainer} from "./style"
+import { Container, FooterContainer, HeaderContainer } from "./style";
 
 export default function Events() {
-    return (
-        <Container>
-            <GlobalStyles/>
-            <HeaderContainer>
-                <Header/>
-            </HeaderContainer>
-            <div className="main-area">
-                <Filter/>
-                <section className="cards-area">
-                    <div className="cards">
-                        <CardsSet cards={data}/>
-                    </div>
-                </section>
-            </div>
-            <FooterContainer>
-                <Footer/>
-            </FooterContainer>
-        </Container>
-    )
+  return (
+    <Container>
+      <GlobalStyles />
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <div className="main-area">
+        <Filter />
+        <section className="cards-area">
+          <div className="cards">
+            <CardsSet cards={fakeEvents} />
+          </div>
+        </section>
+      </div>
+      <FooterContainer>
+        <Footer />
+      </FooterContainer>
+    </Container>
+  );
 }
