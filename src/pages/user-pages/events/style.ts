ac2e192c3454a7cfc24985dc4497ab7d32c53d2a
NicolasFCruvinel <nicolasfcmsouza@gmail.com>
2024-03-01 11:12:57 -0300

    HotFix(admin): alterando export da page create/activity para seu próprio conteúdo

--- src/pages/user-pages/events/style.ts
+++ src/pages/user-pages/events/style.ts
@@ -1,37 +1,36 @@
 import { styled } from "styled-components";
 
-export const Container = styled.div` 
+export const Container = styled.div`
+  hr {
+    display: block;
+    margin: 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  }
 
-    hr {
-        display: block;
-        margin: 0;
-        margin-left: auto;
-        margin-right: auto;
-        width:50%;
-    }
+  .events {
+    text-align: center;
+  }
 
-    .events {
-        text-align: center;
-    }
+  .cards-area {
+    background: var(--cor-branco);
+    border-radius: 20px 20px 0 0;
+    margin: 4.5vh 5vw;
+  }
 
-    .cards-area {
-        background: var(--cor-branco);
-        border-radius: 20px 20px 0 0;
-        margin: 4.5vh 5vw;
-    }
-
-    .cards {
-        max-height: 55vh;
-    }
+  .cards {
+    max-height: 55vh;
+  }
 `;
 
 export const HeaderContainer = styled.header`
-    background: var(--degrade-roxo-rosa);
-    color: var(--cor-branco);
+  background: var(--degrade-roxo-rosa);
+  color: var(--cor-branco);
 `;
 
 export const FooterContainer = styled.footer`
-    background: var(--degrade-roxo-rosa);
-    color: var(--cor-preto);
-    height: 15vh;
+  background: var(--degrade-roxo-rosa);
+  color: var(--cor-preto);
+  height: 15vh;
 `;
